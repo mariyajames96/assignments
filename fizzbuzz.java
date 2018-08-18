96bd67c04ea9875a2212da61e03ca0009f2bb7f9
mariyajames96 <mariyajames.mec@gmail.com>
2018-08-18 19:57:48 +0530

    FizzBuzz

---
+++ fizzbuzz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class fizzbuzz{
+	public static void main(String[] args){
+		for(int i=1;i<=100;i++){
+			if(i%3==0 || i%5==0){
+				if(i%3==0)
+					System.out.print("Fizz");
+				if(i%5==0)
+					System.out.print("Buzz");
+			}
+			else
+				System.out.print(i);
+			System.out.println();
+		}
+	}
+}
